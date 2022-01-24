5bbb930e94ecdecd00f55546b55a8ca6a11de404
kinggggg <liweibooooo@126.com>
2022-01-24 09:13:30 +0800

    很简单的多线程程序

---
+++ src/test/java/com/zeek/concurrent/shengsiyuan/concurrent8/concurrency2/MyThreadTest.java
@@ -0,0 +1,41 @@
+package com.zeek.concurrent.shengsiyuan.concurrent8.concurrency2;
+
+/**
+ * @author liweibo03 <liweibo03@kuaishou.com>
+ * Created on 2022-01-23
+ */
+public class MyThreadTest {
+
+    public static void main(String[] args) {
+        Runnable r = new MyThread();
+
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.start();
+        t2.start();
+    }
+}
+
+class MyThread implements Runnable {
+
+    int x;
+
+    @Override
+    public void run() {
+        x = 0;
+
+        while (true) {
+            System.out.println("result: " + x++);
+
+            try {
+                Thread.sleep((long)(Math.random() * 1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (x == 30) {
+                break;
+            }
+        }
+    }
+}
