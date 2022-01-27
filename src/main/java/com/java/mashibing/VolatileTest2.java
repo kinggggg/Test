926db636a5c62e096b59b92a188e45a38ddbc9eb
kinggggg <liweibooooo@126.com>
2022-01-27 19:58:32 +0800

    留下一个疑问：当加上了这行代码后，无论flog变量被volatile修饰还是不被volatile修饰，while循环都可以结束！！！

---
+++ src/main/java/com/java/mashibing/VolatileTest2.java
@@ -0,0 +1,31 @@
+package com.java.mashibing;
+
+/**
+ * @author liweibo03 <liweibo03@kuaishou.com>
+ * Created on 2022-01-27
+ */
+public class VolatileTest2 {
+
+    // 使用这行代码的时候，由于volatile修饰的变量可以保证当前线程对共享变量修改对其他线程的可见性
+    // 程序不会在while语句造成死循环
+//    public static volatile boolean flag = true;
+    // 使用这行代码时，程序会陷入while语句的无限循环中，因为主线程中对flag变量的修改对server
+    // 线程来说其实是不可见的
+    public static boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(() -> {
+            while(flag) {
+                // 留下一个疑问：当加上了这行代码后，无论flog变量被volatile修饰还是不被volatile修饰，while循环都可以结束！！！
+                System.out.println(Thread.currentThread().getName() + " " + "do>>>");
+            }
+            System.out.println(Thread.currentThread().getName() + " " + "end");
+        },"server").start();
+
+        Thread.sleep(1000);
+
+        System.out.println(Thread.currentThread().getName());
+        flag = false;
+    }
+}
