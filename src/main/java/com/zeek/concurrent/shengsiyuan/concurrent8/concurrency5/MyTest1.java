ac28cc5f09276992a193649b851ae941ada560fc
kinggggg <liweibooooo@126.com>
2022-01-28 23:08:55 +0800

    CountDownLatch简单介绍。增加代码注释

--- src/main/java/com/zeek/concurrent/shengsiyuan/concurrent8/concurrency5/MyTest1.java
+++ src/main/java/com/zeek/concurrent/shengsiyuan/concurrent8/concurrency5/MyTest1.java
@@ -4,6 +4,13 @@ import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
 
 /**
+ * 简单介绍CountDownLatch的使用。
+ *
+ * 需要注意的有以下几点
+ * 1. 当调用countDown将CountDownLatch的计数减为0后，若此还有另外一个线程调用await方法的话，await将直接返回
+ * 2. 当CountDownLatch计数不减为0的话，调用await方法的线程将一直等待下去，除非调用了其interrupt方法
+ * 3. 可以调用带超时时间的await方法，防止出现一直等待的情况出现
+ *
  * @ClassName MyTest1
  * @Author liweibo
  * @Date 2022/1/28 10:20 下午
