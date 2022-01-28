fc171888465f84bd74340a4376d2ef5d6e2d5e58
kinggggg <liweibooooo@126.com>
2022-01-29 00:26:04 +0800

    commit

---
+++ src/main/java/com/zeek/concurrent/shengsiyuan/concurrent8/concurrency6/MyTest1.java
@@ -0,0 +1,12 @@
+package com.zeek.concurrent.shengsiyuan.concurrent8.concurrency6;
+
+/**
+ *
+ *
+ * @ClassName MyTest1
+ * @Author liweibo
+ * @Date 2022/1/29 12:24 上午
+ * @Version v1.0
+ **/
+public class MyTest1 {
+}
