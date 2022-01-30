d0b502ea406e5b9e0b465400cc3a20f399f4e59e
kinggggg <liweibooooo@126.com>
2022-01-30 11:06:54 +0800

    修改包名称

---
+++ src/main/java/com/zeek/concurrent/shengsiyuan/concurrency/concurrency1/MyTest1.java
@@ -0,0 +1,33 @@
+package com.zeek.concurrent.shengsiyuan.concurrency.concurrency1;
+
+/**
+ * 在调用wait方法时, 线程必须要持有被调用对象的锁, 当调用wait方法后, 线程就会释放掉该对象的锁(monitor).
+ * 在调用Thread类的sleep方法时, 线程是不会释放掉对象的锁的
+ *
+ * 关于当调用wait与notify和notifyAll方法的总结
+ *
+ * 1. 当调用wait时, 首先需要确保调用了wait方法的线程已经持有了对象的锁
+ * 2. 当调用wait后, 该线程就会释放掉这个对象的锁, 然后进入到等待状态(wait set)
+ * 3. 当线程调用了wait后进入到等待状态时, 它就可以等待其他其他线程调用相同对象的notify或notifyAll方法来使得自己被唤醒
+ * 4. 一旦这个线程被其他线程唤醒后, 该线程就会与其他线程一同开始竞争这个对象的锁(公平竞争); 只有当该线程获取到这个对象的锁后, 线程才会继续往下执行
+ * 5. 调用wait方法的代码片段需要放在一个synchronized块是synchronized方法中, 这样才可以确保线程在调用wait方法前已经获取到了对象的锁
+ * 6. 当调用对象的notify方法时, 它会随机唤醒该对象等待集合(wait set)中的任意一个线程, 当某个线程(称之为T)被唤醒后, 它就会与其他线程一同竞争对象的锁. 注意!! 这里所说的"它就会与其他线程一同竞争对象的锁"
+ *      其中表达了一层之前自己所忽略了一种含义: T线程被唤醒时(即, 执行wait方法时T线层会进行等待状态), T线程会与其他线程一同竞争对象的锁.
+ *      若竞争成功获取到对象的锁了, 那么T会正常执行其下文的代码;
+ *      若竞争失败了没有获取到对象的锁, 那么T线程还是会停止在执行wait方法一行代码处, 其会等待下一次时机来获取运行
+ * 7. 当调用对象的notifyAll方法时, 它会唤醒该对象等待集合(wait set)中的所有线程, 这些线程被唤醒后, 又会开始竞争对象的锁
+ * 8. 在某一时刻, 只有唯一一个线程可以拥有对象的锁
+ *
+ * @author liweibo03 <liweibo03@kuaishou.com>
+ * Created on 2022-01-23
+ */
+public class MyTest1 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Object object = new Object();
+
+        synchronized (object) {
+            object.wait();
+        }
+    }
+}
---
+++ src/main/java/com/zeek/concurrent/shengsiyuan/concurrency/concurrency7/MyTest2.java
@@ -0,0 +1,63 @@
+package com.zeek.concurrent.shengsiyuan.concurrency.concurrency7;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author liweibo03 <liweibo03@kuaishou.com>
+ * Created on 2022-01-29
+ */
+public class MyTest2 {
+    public static void main(String[] args) {
+        /*
+        程序输出为：
+        ForkJoinPool.commonPool-worker-9---supplyAsync
+        ForkJoinPool.commonPool-worker-9---thenApplyAsync
+        hello world
+
+        虽然在supplyAsync中等待了5秒，但是还是需要supplyAsync执行完了后，thenApplyAsync才继续执行。
+        即，当supplyAsync中的异步任务执行完了后，此开始执行thenApplyAsync中的异步任务
+         */
+        String result = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "---supplyAsync");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "hello";
+        }).thenApplyAsync(value -> {
+            System.out.println(Thread.currentThread().getName() + "---thenApplyAsync");
+            return value + " world";
+        }).join();
+        System.out.println(result);
+
+        System.out.println("====================");
+
+        CompletableFuture.supplyAsync(() -> "hello").thenAccept(value -> System.out.println("welcome " + value));
+
+        System.out.println("====================");
+
+        /*
+        两个supplyAsync中的任务是同时进行的！
+         */
+        String result2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "---supplyAsync1");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "hello";
+        }).thenCombine(CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + "---supplyAsync2");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "world";
+        }), (s1, s2) -> s1 + " " + s2).join();
+        System.out.println(result2);
+
+    }
+}
