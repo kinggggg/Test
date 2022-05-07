faf64f003371043222d4f11cd44c75a1ee5b106c
kinggggg <liweibooooo@126.com>
2022-05-07 17:38:00 +0800

    mockito stubbing学习

---
+++ src/test/java/com/zeek/mockito/lesson04/StubbingTest.java
@@ -0,0 +1,68 @@
+package com.zeek.mockito.lesson04;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StubbingTest {
+
+    private List<String> list;
+
+    @Before
+    public void init() {
+        this.list = mock(ArrayList.class);
+    }
+
+    @Test
+    public void howToUseStubbing() throws Exception {
+        when(list.get(0)).thenReturn("first");
+        assertThat(list.get(0), equalTo("first"));
+
+        when(list.get(anyInt())).thenThrow(new RuntimeException());
+        try {
+            list.get(0);
+            fail();
+        } catch (Exception e) {
+            assertThat(e, instanceOf(RuntimeException.class));
+        }
+    }
+
+    @Test
+    public void howToStubbingVoidMethod() throws Exception {
+        doNothing().when(list).clear();
+        list.clear();
+        verify(list, times(1)).clear();
+
+        doThrow(RuntimeException.class).when(list).clear();
+        try {
+            list.clear();
+            fail();
+        }catch (Exception e) {
+            assertThat(e, instanceOf(RuntimeException.class));
+        }
+    }
+
+    @After
+    public void destroy() {
+        reset(this.list);
+    }
+}
