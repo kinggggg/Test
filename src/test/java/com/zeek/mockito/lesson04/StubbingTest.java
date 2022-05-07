29f5506ff0009ddb588cf7793c87928442f01d08
kinggggg <liweibooooo@126.com>
2022-05-07 17:57:39 +0800

    mockito stubbing学习

--- src/test/java/com/zeek/mockito/lesson04/StubbingTest.java
+++ src/test/java/com/zeek/mockito/lesson04/StubbingTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
@@ -20,7 +21,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StubbingTest {
@@ -56,9 +59,58 @@ public class StubbingTest {
         try {
             list.clear();
             fail();
-        }catch (Exception e) {
+        } catch (Exception e) {
             assertThat(e, instanceOf(RuntimeException.class));
         }
+    }
+
+    @Test
+    public void stubbingDoReturn() throws Exception {
+        when(list.get(0)).thenReturn("first");
+        doReturn("second").when(list).get(1);
+        assertThat(list.get(0), equalTo("first"));
+        assertThat(list.get(1), equalTo("second"));
+    }
+
+    @Test
+    public void iterateStubbing() {
+//        when(list.size()).thenReturn(1,2, 3, 4);
+        // 与上面等价
+        when(list.size()).thenReturn(1).thenReturn(2).thenReturn(3).thenReturn(4);
+
+        assertThat(list.size(), equalTo(1));
+        assertThat(list.size(), equalTo(2));
+        assertThat(list.size(), equalTo(3));
+        assertThat(list.size(), equalTo(4));
+        assertThat(list.size(), equalTo(4));
+    }
+
+    @Test
+    public void stubbingWithAnswer() {
+        when(list.get(anyInt())).thenAnswer(invocationOnMock -> {
+            Integer index = invocationOnMock.getArgument(0, Integer.class);
+            return String.valueOf(index * 10);
+        });
+
+        assertThat(list.get(0), equalTo("0"));
+        assertThat(list.get(999), equalTo("9990"));
+
+    }
+
+    @Test
+    public void stubbingWithRealCallTest() {
+        StubbingService service = mock(StubbingService.class);
+//        System.out.println(service.getClass());
+//        service.getS();
+//
+//        System.out.println(service.getI());
+
+        when(service.getS()).thenReturn("alex");
+        assertThat(service.getS(), equalTo("alex"));
+
+        when(service.getI()).thenCallRealMethod();
+        assertThat(service.getI(), equalTo(10));
+
     }
 
     @After
