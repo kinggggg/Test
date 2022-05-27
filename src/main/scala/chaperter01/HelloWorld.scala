8c0123b1f7fbf9a853e924c3271291df0bc24721
kinggggg <liweibo2058@126.com>
2022-05-27 10:10:54 +0800

    Scala学习

--- src/main/scala/chaperter01/HelloWorld.scala
+++ src/main/scala/chaperter01/HelloWorld.scala
@@ -11,5 +11,59 @@ object HelloWorld {
      */
     println("Hello World")
     System.out.println("Hello World")
+
+    // 5. 空类型
+    // 5.1 空值Unit
+    def m1(): Unit = {
+      println("Hello World");
+    }
+    val a: Unit = m1()
+    println("a: " + a)
+    // 5.2 空引用Null
+    //val n: Int = null // 值类型不能被赋值为null
+    // 5.3 Nothing
+    def m2_1(n: Int): Nothing = {
+      // 返回值是Nothing的话, 必须抛出异常
+        throw new NullPointerException
+    }
+    // 返回值的类型是Int, 因为Nothing类型是所有子类型的子类
+    def m2(n: Int): Int = {
+      if(n == 1) {
+        throw new NullPointerException
+      }else {
+        return n
+      }
+    }
+    val b = m2(0)
+    println(b)
+
+    println("============================")
+    // 函数作为参数传递
+    def f1(f: (Int, Int) => Int): Int = {
+      f(2, 4)
+    }
+    def add(a: Int, b: Int): Int = a + b
+    println(f1(add))
+    println(f1(add _))
+
+    println("============================")
+    def f = () => {
+      println("f...")
+      10
+    }
+    // 将f()的执行结果作为参数传递给foo1
+    foo1(f())
+    // 将f自身作为参数传递给foo2
+    foo2(f())
+    // 传值函数
+    def foo1(a: Int): Unit = {
+      println("foo1")
+      println(a)
+    }
+    // 传名函数
+    def foo2(a: =>Int): Unit = {
+      println("foo2")
+      println(a)
+    }
   }
 }
