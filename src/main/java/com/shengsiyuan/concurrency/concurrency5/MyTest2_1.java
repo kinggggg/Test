47c281e352dc7f5c17e75bafeacdff5e68fbad3f
liweibo04 <liweibo04@58.com>
2025-01-09 11:05:07 +0800

    修改注释

--- src/main/java/com/shengsiyuan/concurrency/concurrency5/MyTest2_1.java
+++ src/main/java/com/shengsiyuan/concurrency/concurrency5/MyTest2_1.java
@@ -11,7 +11,7 @@ import java.util.concurrent.CyclicBarrier;
  * 1. 初始化CyclicBarrier中的各种成员变量，包括parties、count以及Runnable（可选）
  * 2. 当调用await方法时，底层会先检查计数器是否已经归零，如果是的话，那么就首先执行可选的Runnable，接下来开始想一下generation
  * 3. 在下一个分代中，将会重置count的值为parties，并且创建新的Generation实例
- * 4. 同事会调用Condition的signalAll方法，唤醒所有在屏障前面等待的线程，让其开始执行
+ * 4. 同时会调用Condition的signalAll方法，唤醒所有在屏障前面等待的线程，让其开始执行
  * 5. 如果计数器没有归零，那么当前的调用线程将会通过Condition的await方法，在屏障前进行等待
  * 6. 以上所有执行流程均在lock锁的控制范围内，不会出现并发情况
  * @ClassName MyTest2
